73c30cf09a2a30f0df5c30c5d2a68d9c4a16e769
Mitsaqy <mitsaqyanbiya@gmail.com>
2024-09-19 12:19:36 +0700

    toggle menu mobile

--- src/components/ListMenu.tsx
+++ src/components/ListMenu.tsx
@@ -1,45 +1,60 @@
 'use client'
-
 import React, { useState, useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import TransitionLink from './TransitionLink'
 
 const ListMenu: React.FC = () => {
-  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false)
-  const menuRef = useRef<HTMLDivElement>(null)
+    const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false)
+    const menuRef = useRef<HTMLDivElement>(null)
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
 
-  const toggleMenu = () => {
-    setIsMenuVisible(prevState => !prevState)
-  }
+    useEffect(() => {
+        const handleResize = () => {
+        setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        handleResize()
+        return () => {
+        window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
-  useEffect(() => {
-    if (window.innerWidth > 400 ) setIsMenuVisible(true)
-  }, [window.innerWidth])
+    useEffect(() => {
+        if (windowWidth > 400) {
+            setIsMenuVisible(true)
+        } else {
+            setIsMenuVisible(false)
+        }
+    }, [windowWidth])
 
-  useEffect(() => {
-    if (menuRef.current) {
-      if (isMenuVisible) {
-        gsap.to(menuRef.current, { opacity: 1, y: 0, duration: 0.3,})
-      } else {
-        gsap.to(menuRef.current, { opacity: 0, y: 20, duration: 0.3 })
-      }
+    const toggleMenu = () => {
+        setIsMenuVisible(prevState => !prevState)
     }
-  }, [isMenuVisible])
 
-  return (
+    useEffect(() => {
+    if (menuRef.current) {
+        if (isMenuVisible) {
+        gsap.to(menuRef.current, { opacity: 1, y: 0, duration: 0.3,})
+        } else {
+        gsap.to(menuRef.current, { opacity: 0, y: 20, duration: 0.3 })
+        }
+    }
+    }, [isMenuVisible])
+
+    return (
     <div className="link__menu">
-      <div className="link__icon" onClick={toggleMenu}>
+        <div className="link__icon" onClick={toggleMenu}>
 
-      </div>
-      <div className="link__wrapper" ref={menuRef}>
+        </div>
+        <div className="link__wrapper" ref={menuRef}>
         <TransitionLink href="/where" label="where?" />
         <TransitionLink href="/who" label="who?" />
         <TransitionLink href="/what" label="what?" />
-      </div>
+        </div>
     </div>
 
-    
-  )
+
+    )
 }
 
 export default ListMenu
