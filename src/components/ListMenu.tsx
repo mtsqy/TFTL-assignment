aa38ad5aba9cb8f0fcac3b46a0cbd758541723dd
Mitsaqy <mitsaqyanbiya@gmail.com>
2024-09-19 12:24:31 +0700

    window ssr issue

--- src/components/ListMenu.tsx
+++ src/components/ListMenu.tsx
@@ -1,60 +1,61 @@
-'use client'
+"use client"
 import React, { useState, useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import TransitionLink from './TransitionLink'
 
 const ListMenu: React.FC = () => {
-    const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false)
-    const menuRef = useRef<HTMLDivElement>(null)
-    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
+  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false)
+  const [windowWidth, setWindowWidth] = useState<number>(0)
+  const menuRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const handleResize = () => {
+  const toggleMenu = () => {
+    setIsMenuVisible(prevState => !prevState)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
         setWindowWidth(window.innerWidth)
-        }
-        window.addEventListener('resize', handleResize)
-        handleResize()
-        return () => {
+      }
+      handleResize()
+      window.addEventListener('resize', handleResize)
+      return () => {
         window.removeEventListener('resize', handleResize)
-        }
-    }, [])
-
-    useEffect(() => {
-        if (windowWidth > 400) {
-            setIsMenuVisible(true)
-        } else {
-            setIsMenuVisible(false)
-        }
-    }, [windowWidth])
-
-    const toggleMenu = () => {
-        setIsMenuVisible(prevState => !prevState)
+      }
     }
+  }, [])
 
-    useEffect(() => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (windowWidth > 400) {
+        setIsMenuVisible(true)
+      } else {
+        setIsMenuVisible(false)
+      }
+    }
+  }, [windowWidth])
+
+  useEffect(() => {
     if (menuRef.current) {
-        if (isMenuVisible) {
-        gsap.to(menuRef.current, { opacity: 1, y: 0, duration: 0.3,})
-        } else {
-        gsap.to(menuRef.current, { opacity: 0, y: 20, duration: 0.3 })
-        }
+      if (isMenuVisible) {
+        gsap.to(menuRef.current, { opacity: 1, y: 0, duration: 0.3, display: 'block' })
+      } else {
+        gsap.to(menuRef.current, { opacity: 0, y: -20, duration: 0.3, display: 'none' })
+      }
     }
-    }, [isMenuVisible])
+  }, [isMenuVisible])
 
-    return (
+  return (
     <div className="link__menu">
-        <div className="link__icon" onClick={toggleMenu}>
-
-        </div>
-        <div className="link__wrapper" ref={menuRef}>
+      <div className="link__icon" onClick={toggleMenu}>
+      </div>
+      <div className="link__wrapper" ref={menuRef}>
         <TransitionLink href="/where" label="where?" />
         <TransitionLink href="/who" label="who?" />
         <TransitionLink href="/what" label="what?" />
-        </div>
+      </div>
     </div>
-
-
-    )
+  )
 }
 
 export default ListMenu
